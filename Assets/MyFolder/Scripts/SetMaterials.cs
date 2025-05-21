5d31e5c05ba6bb3e74b9c3658ca93b002960c65b
wlsrb0147 <50743287+wlsrb0147@users.noreply.github.com>
2025-05-21 18:56:04 +0900

    123

--- Assets/MyFolder/Scripts/SetMaterials.cs
+++ Assets/MyFolder/Scripts/SetMaterials.cs
@@ -4,6 +4,7 @@ using Cysharp.Threading.Tasks.Triggers;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
+using DG.Tweening;
 
 public class SetMaterials : MonoBehaviour
 {
@@ -11,8 +12,40 @@ public class SetMaterials : MonoBehaviour
     [SerializeField] private SkinnedMeshRenderer[] meshRenderers;
     [SerializeField] private Material bodyMat;
     [SerializeField] private Material faceMat;
-
+    
     private Material _newMaterial;
+
+    private int _maxClick;
+
+    private int _currentClick;
+    private int CurrentClick
+    {
+        get => _currentClick;
+        set
+        {
+            _currentClick =  value;
+            if (_currentClick >= _maxClick)
+            {
+                _canClick = false;
+            }
+        }
+    }
+    
+    private bool _canClick = true;
+
+    private bool CanClick
+    {
+        get => _canClick;
+        set
+        {
+            if (true)
+            {
+                
+            }
+            _canClick = value;
+        }
+    }
+
 
     private void OnEnable()
     {
@@ -70,7 +103,7 @@ public class SetMaterials : MonoBehaviour
         
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         if (_newMaterial)
         {
@@ -80,7 +113,41 @@ public class SetMaterials : MonoBehaviour
 
     public void Clicked()
     {
-        AudioManager.Instance.PlayOneShotAudio(AudioName.Touch);
+        if (_canClick)
+        {
+            AudioManager.Instance.PlayOneShotAudio(AudioName.Touch);
+            PlayScaleBounce().Forget();
+            ++CurrentClick;
+        }
+
+    }
+    
+    private float scaleUpPercent = 1.5f;  // 150%
+    private float scaleDownPercent = 1.05f;  // 105%
+    private float scaleUpDuration = 0.3f;
+    private float scaleDownDuration = 0.2f;
+    private Ease scaleUpEase = Ease.OutQuad;
+    private Ease scaleDownEase = Ease.OutBack;
+
+    private Tween _currentTween;
+    private Vector3 _initialScale;
+
+    [SerializeField] private int val = 150;
+    
+    private async UniTaskVoid PlayScaleBounce()
+    {
+        _canClick = false;
+        _currentTween?.Kill(); // 기존 Tween 제거
+
+        await UniTask.Delay(val);
         
+		_initialScale = transform.localScale;
+        _currentTween = transform.DOScale(_initialScale * scaleUpPercent, scaleUpDuration)
+            .SetEase(scaleUpEase)
+            .OnComplete(() =>
+            {
+                transform.DOScale(_initialScale * scaleDownPercent, scaleDownDuration)
+                    .SetEase(scaleDownEase).OnComplete(() => _canClick = true);
+            });
     }
 }
--- Assets/MyFolder/Scripts/SpawnModel.cs
+++ Assets/MyFolder/Scripts/SpawnModel.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class SpawnModel : MonoBehaviour
 {
     [SerializeField] private GameObject[] model;
-
+    [SerializeField] private Transform[] spawnPoints;
+    
     [SerializeField] private int value;
 
     public void ChangeFace(Texture2D texture)
     {
         int val = Random.Range(0, model.Length);
+        
+        int pos = Random.Range(0, spawnPoints.Length);
+        
         val = 0;
-        GameObject prefab = Instantiate(model[value]);
+        GameObject prefab = Instantiate(model[value], spawnPoints[pos].position, spawnPoints[pos].rotation);
         
         SetMaterials setMat = prefab.GetComponent<SetMaterials>();
         
@@ -21,7 +25,9 @@ public class SpawnModel : MonoBehaviour
     {
         int val = Random.Range(0, model.Length);
         val = 0;
-        GameObject prefab = Instantiate(model[value]);
+        
+        int pos = Random.Range(0, spawnPoints.Length);
+        GameObject prefab = Instantiate(model[value], spawnPoints[pos].position, spawnPoints[pos].rotation);
         
         SetMaterials setMat = prefab.GetComponent<SetMaterials>();
         
